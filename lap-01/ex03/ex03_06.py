cf816f5c72a60276b6ef4e01959a9fa206963831
nvthih24 <nguyenvanthinh3434mc2@gmail.com>
2025-03-07 13:16:18 +0700

    [add] lap-01 ex01 ex02 ex03

--- lap-01/ex03/ex03_06.py
+++ lap-01/ex03/ex03_06.py
@@ -1,1 +1,14 @@
-# This is a blank file.
+def xoa_phan_tu(dictionary, key):
+    if key in dictionary:
+        del dictionary[key]
+        return True
+    else:
+        return False
+
+my_dict = {'a': 1, 'b': 2, 'c': 3,'d': 4}
+key_to_delete ='b'
+result = xoa_phan_tu(my_dict, key_to_delete)
+if result:
+    print("Phần tử đã được xóa từ Dictionary:",my_dict)
+else:
+    print("Không tìm thầy phần từ cần xóa trong Dictionary.")
